417d2a13fa698f7d008ec9a6819465ba6b657b88
hdorgeval <henri.dorgeval@outlook.fr>
2019-09-01 16:07:25 +0200

    test(grap-generator): rename test

--- src/tools/markdown-to-graph-data/tests/story-with-only-h1-headings/create-graph-data.ts
+++ src/tools/markdown-to-graph-data/tests/story-with-only-h1-headings/create-graph-data.ts
@@ -1,5 +1,5 @@
 import { convertMarkdownToGraphData } from '../../graph-generator';
 
-const storyFile = `${__dirname}/story-with-only-h1-heading.md`;
+const storyFile = `${__dirname}/story-with-only-h1-headings.md`;
 const targetDirectory = `${process.cwd()}/src/app/data`;
 convertMarkdownToGraphData(storyFile, targetDirectory);
