2c26c449b277ace2a4d2dafd55b9369391eab1c6
hdorgeval <henri.dorgeval@outlook.fr>
2019-09-01 16:22:21 +0200

    test(grap-generator): dynamically get the markdown file in the test directory

--- src/tools/fs/index.ts
+++ src/tools/fs/index.ts
@@ -1,2 +1,3 @@
 export * from './get-directories-in';
+export * from './get-files-in-directory';
 export * from './read-all-lines-in-file';
--- src/tools/markdown-to-graph-data/tests/story-with-only-h1-headings/create-graph-data.ts
+++ src/tools/markdown-to-graph-data/tests/story-with-only-h1-headings/create-graph-data.ts
@@ -1,5 +1,11 @@
 import { convertMarkdownToGraphData } from '../../graph-generator';
+import { getFilesInDirectory } from '../../../fs';
 
-const storyFile = `${__dirname}/story-with-only-h1-headings.md`;
-const targetDirectory = `${process.cwd()}/src/app/data`;
-convertMarkdownToGraphData(storyFile, targetDirectory);
+const storyFile = getFilesInDirectory(`${__dirname}`, (path): boolean =>
+  path.endsWith('.md'),
+).shift();
+
+if (storyFile) {
+  const targetDirectory = `${process.cwd()}/src/app/data`;
+  convertMarkdownToGraphData(storyFile, targetDirectory);
+}
