37b7e94a4c4969217f87126d15c4f89ed004a94a
hdorgeval <henri.dorgeval@outlook.fr>
2019-09-01 16:03:32 +0200

    test(grap-generator): refactor test

--- src/tools/markdown-to-graph-data/tests/h1-headings/story-with-only-h1-heading/create-graph-data.ts
+++
@@ -1,5 +0,0 @@
-import { convertMarkdownToGraphData } from '../../../graph-generator';
-
-const storyFile = `${__dirname}/story-with-only-h1-heading.md`;
-const targetDirectory = `${process.cwd()}/src/app/diagrams/flow-diagram/data`;
-convertMarkdownToGraphData(storyFile, targetDirectory);
--- src/tools/markdown-to-graph-data/tests/story-with-only-h1-heading/story-with-only-h1-heading.test.ts
+++ src/tools/markdown-to-graph-data/tests/story-with-only-h1-heading/story-with-only-h1-heading.test.ts
@@ -1,6 +1,6 @@
 import 'jest';
-import * as SUT from '../../../graph-generator';
-import { StoryNodeType } from '../../../../../story-types';
+import * as SUT from '../../graph-generator';
+import { StoryNodeType } from '../../../../story-types';
 describe('Markdown Boundaries', (): void => {
   test('Should get H1 headings', (): void => {
     // Given
@@ -12,11 +12,15 @@ describe('Markdown Boundaries', (): void => {
     // Then
     const expectedTypeOfRootNode: StoryNodeType = 'start of period';
     expect(data.nodes[0].id).toBe('root');
+    expect(data.nodes[0].weight).toBe(30);
     expect(data.nodes[0].type).toBe(expectedTypeOfRootNode);
     expect(data.nodes.length).toBe(4);
     expect(data.links.length).toBe(3);
     expect(data.links[0].source.id).toBe('root');
+    expect(data.links[0].weight).toBe(10);
     expect(data.links[1].source.id).toBe('root');
+    expect(data.links[1].weight).toBe(10);
     expect(data.links[2].source.id).toBe('root');
+    expect(data.links[2].weight).toBe(10);
   });
 });
